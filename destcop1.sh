9d7b7522a462fd24165d1f5c394dcd065b457d79
b1zar <55201519+b1zar@users.noreply.github.com>
2019-09-12 14:33:14 +0400

    Create destcop1.sh

---
+++ destcop1.sh
@@ -0,0 +1,22 @@
+echo Введите имя пользователя
+read NAMEUSER
+#Обновление всех пакетов
+apt update && apt upgrade -y
+#Установка окружения рабочего стола
+apt install ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal -y
+#Создание нового юзера
+adduser $NAMEUSER
+#Установка патека по предоставлению root прав
+apt install sudo
+#Предоставление рут прав созданому юзеру
+adduser $NAMEUSER sudo
+#Установка репозитория NoMachine (Аналог RPD только для Linux)
+cd /tmp && wget https://download.nomachine.com/download/6.7/Linux/nomachine_6.7.6_11_amd64.deb
+#Установка NoMachine
+dpkg -i nomachine_6.7.6_11_amd64.deb
+#Установка репозитория для скачивания Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+#Установка Google Chrome
+sudo apt-get update
+sudo apt-get install google-chrome-stable -y
